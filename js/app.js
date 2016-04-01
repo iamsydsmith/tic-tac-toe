be38615a92177ef58a59bc639e0c3191c0172af6
Sydney Smith <sydmac@gmail.com>
2016-04-01 16:16:10 -0400

    click event, render moves in cell & array, counter

--- js/app.js
+++ js/app.js
@@ -2,9 +2,28 @@ $(document).ready(function() {
 
 
     var $gameCells = $('.cell');
+    var moves = ["", "", "", "","", "", "", "",""]
+    var count = 0;
+    var turn = 'X';
 
     $($gameCells).one('click', function() {
-        console.log(this.id);
+        $(this).html(turn);
+        moves[this.id] = turn;
+        count++;
+
+        //turn = (turn = 'X') ? 'O' : 'X';
+
+        if(count % 2 === 0){
+          turn = 'X';
+        } else {
+          turn = 'O';
+        }
+
+
+
+        console.log(moves, count, turn);
+
+
     })
 
 
