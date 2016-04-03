ba07f6928f69cf27e79923a5a16b6427fcd27cd3
Sydney Smith <sydmac@gmail.com>
2016-04-02 21:32:51 -0400

    styled the tic-tac-toe board with css

--- js/app.js
+++ js/app.js
@@ -9,67 +9,69 @@ $(document).ready(function() {
     //clicking event, puts 'X' or 'O' in box. Switches from 'X' to 'O' on if each turn, plus incrementing the turn counter
     function startGame() {
         $($gameCells).one('click', function() {
-        $(this).html(turn);
-        moves[this.id] = turn;
-        count++;
-        if (count % 2 === 0) {
-            turn = 'X'; //X turn
-            checkWinner('O');
-        } else {
-            turn = 'O'; //O turn
-            checkWinner('X');
-        }
-
-    });
-  }
-
-startGame();
-    // checks to see if there is a possibility of a winner
-    function checkWinner(player) {
-        if (moves[0] === player && moves[1] === player && moves[2] === player) {
-            alert('Game over! ' + player + ' is the winner!');
-            $('#newGame').removeClass('invisible');
-        } else if (moves[3] === player && moves[4] === player && moves[5] === player) {
-            alert('Game over! ' + player + ' is the winner!');
-            $('#newGame').removeClass('invisible');
-        } else if (moves[6] === player && moves[7] === player && moves[8] === player) {
-            alert('Game over! ' + player + ' is the winner!');
-            $('#newGame').removeClass('invisible');
-        } else if (moves[0] === player && moves[3] === player && moves[6] === player) {
-            alert('Game over! ' + player + ' is the winner!');
-            $('#newGame').removeClass('invisible');
-        } else if (moves[1] === player && moves[4] === player && moves[7] === player) {
-            alert('Game over! ' + player + ' is the winner!');
-            $('#newGame').removeClass('invisible');
-        } else if (moves[2] === player && moves[5] === player && moves[8] === player) {
-            alert('Game over! ' + player + ' is the winner!');
-            $('#newGame').removeClass('invisible');
-        } else if (moves[0] === player && moves[4] === player && moves[8] === player) {
-            alert('Game over! ' + player + ' is the winner!');
-            $('#newGame').removeClass('invisible');
-        } else if (moves[2] === player && moves[4] === player && moves[6] === player) {
-            alert('Game over! ' + player + ' is the winner!');
-            $('#newGame').removeClass('invisible');
-        } else if (count === 9) {
-            alert("It's a tie");
-            $('#newGame').removeClass('invisible');
-        }
+            $(this).html(turn);
+            moves[this.id] = turn;
+            count++;
+            if (count % 2 === 0) {
+                turn = 'X'; //X turn
+                checkWinner('O');
+            } else {
+                turn = 'O'; //O turn
+                checkWinner('X');
+            }
+        });
     }
 
-  function turnOffBoard() {
-    $('.cell').off('click');
-    moves = ["", "", "", "", "", "", "", "", ""];
-    turn = 'X';
-    count = 0;
-}
+    startGame();
 
-    //New game button, clears board and resets game
-    var reset = $('#newGame').click(function() {
+    //resets clicks on board and resets variables.
+    function turnOffBoard() {
+        $($gameCells).off('click');
+        moves = ["", "", "", "", "", "", "", "", ""];
+        turn = 'X';
+        count = 0;
+    }
+
+    //New game button, clears board
+    $('#newGame').click(function() {
         $(this).on();
+        $gameCells.removeClass('won');
         $gameCells.html('');
         turnOffBoard();
         startGame();
-
     });
+
+    // checks to see if there is a possibility of a winner
+    function checkWinner(player) {
+        if (moves[0] === player && moves[1] === player && moves[2] === player) {
+            $('#0, #1, #2').addClass('won');
+            alert('Game over! ' + player + ' is the winner!');
+        } else if (moves[3] === player && moves[4] === player && moves[5] === player) {
+            $('#3, #4, #5').addClass('won');
+            alert('Game over! ' + player + ' is the winner!');
+        } else if (moves[6] === player && moves[7] === player && moves[8] === player) {
+            $('#6, #7, #8').addClass('won');
+            alert('Game over! ' + player + ' is the winner!');
+        } else if (moves[0] === player && moves[3] === player && moves[6] === player) {
+            $('#0, #3, #6').addClass('won');
+            alert('Game over! ' + player + ' is the winner!');
+        } else if (moves[1] === player && moves[4] === player && moves[7] === player) {
+            $('#1, #4, #7').addClass('won');
+            alert('Game over! ' + player + ' is the winner!');
+        } else if (moves[2] === player && moves[5] === player && moves[8] === player) {
+            $('#2, #5, #8').addClass('won');
+            alert('Game over! ' + player + ' is the winner!');
+        } else if (moves[0] === player && moves[4] === player && moves[8] === player) {
+            $('#0, #4, #8').addClass('won');
+            alert('Game over! ' + player + ' is the winner!');
+        } else if (moves[2] === player && moves[4] === player && moves[6] === player) {
+            $('#2, #4, #6').addClass('won');
+            alert('Game over! ' + player + ' is the winner!');
+        } else if (count === 9) {
+            alert("It's a tie");
+        }
+    }
+
+
 
 });
