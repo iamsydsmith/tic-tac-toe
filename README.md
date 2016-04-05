cf36e8a599d536b2a763948586dc9717e2ee5959
Sydney Smith <sydmac@gmail.com>
2016-04-05 16:57:38 -0400

    added score functionality

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
-<h1>Tic Tac Toe Projec</h1>
-<h2>Overview</h2>>
+<h1>Tic Tac Toe Project</h1>
+<h2>Overview</h2>
 
 For project #1, I created a simple game of Tic Tae Toe.
 
-Players alternate placing Xs and Os on the game board until either player has three in a row or all nine squares are filled.
+Players alternate placing Xs and Os on the game board until either player has three in a row or all nine squares are filled. Once a player wins, their score increases.
 
+<br><br>
 http://iamsydsmith.github.io/tic-tac-toe/
 
-<h2>Technologies Used:</h2>>
+<h2>Technologies Used:</h2>
 
 <ul>
 <li>Languages - HTML5, CSS3, jQuery</li>
 <li>Design - MockingBird, Google Fonts</li>
-<li>Project Planning & User Stories - <a href="https://trello.com/b/DAYM9Byr/wdi-project-1-tictaetoe" target="_blank">Trello"</a></li>
+<li>Project Planning & User Stories - <a href="https://trello.com/b/DAYM9Byr/wdi-project-1-tictaetoe" target="_blank">Trello</a></li>
 <li>Sublime Text 3</li>
 </ul>
 
-Trello Board: https://trello.com/b/DAYM9Byr/wdi-project-1-tictaetoe
-<h2>Wireframe:</h2>>
+<h2>Wireframe:</h2>
 
 <img src="https://github.com/iamsydsmith/tic-tac-toe/blob/gh-pages/Tic_Tae_Toe_-_wireframe.png">
 
--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ $(document).ready(function() {
     var moves = ["", "", "", "", "", "", "", "", ""]
     var count = 0;
     var turn = 'X';
+    var winner = null;
+    var xWins = 0;
+    var oWins = 0;
+    var tie = 0;
 
     //clicking event, puts 'X' or 'O' in box. Switches from 'X' to 'O' on if each turn, plus incrementing the turn counter
     function startGame() {
@@ -22,12 +26,21 @@ $(document).ready(function() {
         });
     }
 
+    scoreBoard();
     startGame();
+
+          //scoreboard
+    function scoreBoard() {
+        $('.playerX').html(xWins);
+        $('.playerO').html(oWins);
+        $('.Tie').html(tie);
+    }
 
     //resets clicks on board and resets variables.
     function resetBoard() {
         $gameCells.removeClass('won');
         $gameCells.html('');
+        winner = 'null';
         moves = ["", "", "", "", "", "", "", "", ""];
         turn = 'X';
         count = 0;
@@ -42,40 +55,78 @@ $(document).ready(function() {
 
     // checks to see if there is a possibility of a winner
     function checkWinner(player) {
+
+            //score
+    function score (){
+        if(winner === 'X'){
+            xWins++;
+        } else if (winner === 'O'){
+            oWins++;
+        } else {
+            tie++;
+        }
+    }
+
         if (moves[0] === player && moves[1] === player && moves[2] === player) {
             $('#0, #1, #2').addClass('won');
             alert('Game over! ' + player + ' is the winner!');
+            winner = player;
+            score();
+            scoreBoard();
             $($gameCells).off('click');
         } else if (moves[3] === player && moves[4] === player && moves[5] === player) {
             $('#3, #4, #5').addClass('won');
             alert('Game over! ' + player + ' is the winner!');
+            winner = player;
+            score();
+            scoreBoard();
             $($gameCells).off('click');
         } else if (moves[6] === player && moves[7] === player && moves[8] === player) {
             $('#6, #7, #8').addClass('won');
             alert('Game over! ' + player + ' is the winner!');
+            winner = player;
+            score();
+            scoreBoard();
             $($gameCells).off('click');
         } else if (moves[0] === player && moves[3] === player && moves[6] === player) {
             $('#0, #3, #6').addClass('won');
             alert('Game over! ' + player + ' is the winner!');
+            winner = player;
+            score();
+            scoreBoard();
             $($gameCells).off('click');
         } else if (moves[1] === player && moves[4] === player && moves[7] === player) {
             $('#1, #4, #7').addClass('won');
             alert('Game over! ' + player + ' is the winner!');
+            winner = player;
+            score();
+            scoreBoard();
             $($gameCells).off('click');
         } else if (moves[2] === player && moves[5] === player && moves[8] === player) {
             $('#2, #5, #8').addClass('won');
             alert('Game over! ' + player + ' is the winner!');
+            winner = player;
+            score();
+            scoreBoard();
             $($gameCells).off('click');
         } else if (moves[0] === player && moves[4] === player && moves[8] === player) {
             $('#0, #4, #8').addClass('won');
             alert('Game over! ' + player + ' is the winner!');
+            winner = player;
+            score();
+            scoreBoard();
             $($gameCells).off('click');
         } else if (moves[2] === player && moves[4] === player && moves[6] === player) {
             $('#2, #4, #6').addClass('won');
             alert('Game over! ' + player + ' is the winner!');
+            winner = player;
+            score();
+            scoreBoard();
             $($gameCells).off('click');
         } else if (count === 9) {
             alert("It's a tie");
+            score();
+            scoreBoard();
         }
     }
 
